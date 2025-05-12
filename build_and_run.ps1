6c0fe4a9532438e7f9ab696d20894f90d58ccf49
Carlos Tarjano <tesserato@hotmail.com>
2025-05-12 18:24:38 +0100

    changed build_and_run.ps1 to include coverage.out

--- build_and_run.ps1
+++ build_and_run.ps1
@@ -2,4 +2,4 @@ go build .
 
 git describe --tags --abbrev=0
 
-./CodeWeaver -clipboard -ignore="\.git.*,.+\.exe,codebase.md,excluded_paths.txt,DRAFT\.md,coverage"
+./CodeWeaver -clipboard -ignore="\.git.*,.+\.exe,codebase.md,excluded_paths.txt,DRAFT\.md,coverage$"
